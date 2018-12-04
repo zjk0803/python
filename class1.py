1572a9357931398dbd29eb75172f600eff3ab5fd
zjk0803 <43437186+zjk0803@users.noreply.github.com>
2018-12-04 12:49:08 +0800

    Update class1.py

--- class1.py
+++ class1.py
@@ -86,3 +86,22 @@ def crawl_site(url,max_errors=5):
 crawl_site('http://example.python-scraping.com/view/-')
 
 
+
+
+
+
+import golb
+import os
+from PIL import Image
+
+def make_thumbnail(filename):
+    base_filename,file_extension = os.path.splitext(filename)
+    thumbnail_filename = f"{base_filename}_thumbnail{file_extension}"
+    image = Image.open(filename)
+    image.thumbnail(size=(128,128))
+    image.save(thumbnail_filename,"JPEG")
+    return thumbnail_filename
+for image_file in glob.glob("*.jpg"):
+    thumnail_file = make_image_thumbnail(image_file)
+print(f"A thumbnail for {image_file} was save as {thumbnail_file}")
+
